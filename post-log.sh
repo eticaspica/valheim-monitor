e8806b3bc072022bb392874f9552ffa03a072a23
herta <31131731+eticaspica@users.noreply.github.com>
2025-12-30 21:34:17 +0900

    post-log.shを新規作成し、valheim.serviceのログをNotion APIに送信する処理を追加

---
+++ post-log.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -a
+source .env
+set +a
+
+journalctl -u valheim.service -f --since now --no-pager | 
+while read -r line; do
+
+    case "$line" in
+        *"Got character ZDOID"*)
+            ;;
+        *"Destroying abandoned non persistent"*)
+            ;;
+        *"Game server connected"*)
+            ;;
+        *"OnApplicationQuit"*)
+            ;;
+        *"Connections"*)
+            ;;
+        *)
+            continue
+            ;;
+    esac
+
+    json_payload=$(./notion/make-payload.sh "$notion_database_id" "$line")
+
+    ./notion/post-page.sh "$notion_integration_token" "$json_payload"
+    echo
+done
